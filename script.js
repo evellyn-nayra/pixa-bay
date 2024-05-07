c78232c506c82a807b35ebc07c926c753b24378a
Evelyn Nayra <evelynnayra1507@gmail.com>
2024-05-06 21:41:24 -0300

    pixa-bay
    atulizando informações e adicionando um tema ao site

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const video = document.getElementById("myVideo");  //Estamos fazendo uma busca dentro do HTML para salvar o ID
 const btn = document.getElementById("myBtn");
 
-
-
 function myFunction() { //Aqui criamos uma Função 
-  
-  
-if (video.paused) { //se o vídeo estiver pausado execute: Play.
+
+  if (video.paused) { //se o vídeo estiver pausado execute: Play.
     video.play();
     btn.innerHTML = "Pause"; // O innerHTML funciona como uma alteração textual dentro do HTML, usamos ele no Javascript para executar essa chamada.
   } else { //caso contrário, se o 
@@ -15,10 +12,9 @@ if (video.paused) { //se o vídeo estiver pausado execute: Play.
   }
 }
 
-window.revelar = ScrollReveal({reset:true});
+window.revelar = ScrollReveal({ reset: true });
 
 revelar.reveal('.efeito-img-topo', {
-    duration: 3000,
-    distance: '150px'
+  duration: 3000,
+  distance: '150px'
 });
-
